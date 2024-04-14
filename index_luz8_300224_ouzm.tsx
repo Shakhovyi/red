39fe14a5695ebcf87b77f376b271112e246e38f3
Shakhovyi <shahovyii@gmail.com>
2024-04-14 11:03:50 +0300

    Optimized user info variable before file handling

---
+++ index_luz8_300224_ouzm.tsx
@@ -0,0 +1,4 @@
+const capitalizeString = str => str.toUpperCase();
+
+const snakeCaseToCamelCase = str => str.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
+const sumAllNumbers = (...numbers) => numbers.reduce((sum, num) => sum + num, 0);
